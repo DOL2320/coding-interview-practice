b1bc831b30da4b66429a0b7f8866ebaa0175fd79
Tom Caswell <tcaswell@salesforce.com>
2022-05-16 14:05:55 -0400

    Practice Exercise One

---
+++ c-exercises/practice1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	// given an input string from the command line,
+        // rotate the characters three spaces to the right.
+        // for example, the character string "123456"
+        // would become "456123".
+        // you can expect the input length to be a multiple of three.
+        // if the input was not a multiple of three, whatever the
+        // remainder would be after the first multiples of three
+        // would be rotated to the front of the string.
+        // for example, the character string "12345"
+        // would become "45123".
+}
