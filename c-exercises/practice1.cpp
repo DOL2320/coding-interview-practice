8650f971c9c9b27386c78981ac927494a033ca22
DOL2320 <36548016+DOL2320@users.noreply.github.com>
2022-06-04 09:55:31 -0700

    Update practice1.cpp

--- c-exercises/practice1.cpp
+++ c-exercises/practice1.cpp
@@ -14,4 +14,22 @@ int main() {
         // would be rotated to the front of the string.
         // for example, the character string "12345"
         // would become "45123".
+	
+	string input; // Declare a string variable
+	cout << "Enter a string: "; // Ask user for input
+	cin >> input; // Store user input into a variable
+	
+	// Store the first 3 characters in a variable
+	string begin = input.substr(0,3);
+	
+	// Store the end of the string in a variable
+	string end = input.substr(3, input.length() - 1);
+	
+	// Replace the beginning of the string with the end of the string
+	input.replace(0, end.length() - 1, end);
+	
+	// Replace the end of the string with the beginning of the string
+	input.replace(end.length(), input.length() - 1, begin);
+	
+	cout << input;
 }
